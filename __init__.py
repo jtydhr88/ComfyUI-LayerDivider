23c0c0bf015a86f2f877672fc0ceeea1595838da
Terry Jia <terryjia88@gmail.com>
2024-07-05 20:58:06 -0400

    remove try catch for better error info

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
-try:
-    from .layer_divider_node import NODE_CLASS_MAPPINGS
-except:
-    print(f"## ComfyUI LayerDivider - Miss Dependencies.")
-    print(
-        f"## ComfyUI LayerDivider - Please stop ComfyUI first, then run install_windows_portable_win_py311_cu121.bat")
+from .layer_divider_node import NODE_CLASS_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS']
